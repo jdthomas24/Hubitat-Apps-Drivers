844fc4298694771617f88c772013a5574878b1b9
Jason D Thomas <jdthomas24@yahoo.com>
2026-04-09 12:37:03 -0500

    Update Pump Speed Driver.groovy

--- Pentair Intellicenter Control/Pump Speed Driver.groovy
+++ Pentair Intellicenter Control/Pump Speed Driver.groovy
@@ -1,6 +1,6 @@
 // ============================================================
 // Pentair IntelliCenter Pump Driver
-// Version: 1.5.7
+// Version: 1.5.8
 // ============================================================
  
 metadata {
@@ -9,7 +9,7 @@ metadata {
         namespace: "intellicenter",
         author: "jdthomas24",
         description: "Variable speed pump — RPM, watts, GPM and water temperature display",
-        version: "1.5.7"
+        version: "1.5.8"
     ) {
         capability "Switch"
  
