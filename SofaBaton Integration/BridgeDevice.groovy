4c40159ac8a97d7b1538f2f9245186cdfbb00f47
Jason D Thomas <jdthomas24@yahoo.com>
2026-09-15 23:01:36 -0500

    Update comment header for BridgeDevice.groovy

--- SofaBaton Integration/BridgeDevice.groovy
+++ SofaBaton Integration/BridgeDevice.groovy
@@ -1,5 +1,5 @@
 /*
-    Sofabaton Integration Bridge Driver
+    Sofabaton Integration - Bridge Driver
     Copyright 2026 JDThomas. All Rights Reserved
 
     Credits: part of the Sofabaton Integration, whose X1/X1S local receive
