ae07d881b8431ef1e45354b6e0901c48259be83d
Jason D Thomas <jdthomas24@yahoo.com>
2026-09-21 12:18:03 -0500

    Update version number to 1.5.2

--- Reolink Integration/StandaloneDevices.groovy
+++ Reolink Integration/StandaloneDevices.groovy
@@ -1,6 +1,6 @@
 /**
  * Reolink Standalone Devices (Internal Group Driver)
- * Version: 1.5.1
+ * Version: 1.5.2
  *
  * NOT user-facing. Created and managed automatically by the Reolink
  * Integration parent app -- exactly ONE instance total, shared across every
