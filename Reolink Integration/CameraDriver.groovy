e2daf08030e58305339dde7d32c2685801cdf792
Jason D Thomas <jdthomas24@yahoo.com>
2026-09-23 07:22:32 -0500

    Update version number to 1.5.4

--- Reolink Integration/CameraDriver.groovy
+++ Reolink Integration/CameraDriver.groovy
@@ -1,6 +1,6 @@
 /**
  * Reolink Camera (Component Driver)
- * Version: 1.5.3
+ * Version: 1.5.4
  *
  * Thin device: no HTTP of its own. Delegates everything to the parent app via
  * parent.componentX(this, ...), using data values sourceId/channel to
